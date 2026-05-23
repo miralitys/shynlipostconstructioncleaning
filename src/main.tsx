b4dc5d1a5fc03a3955643eeb71d62fa97b95155d
Ramis <miralitys@gmail.com>
2026-05-23 10:50:41 -0500

    Defer homepage React work

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function renderApp() {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+const isHome = window.location.pathname === '/'
+
+if (isHome) {
+  const start = () => {
+    window.removeEventListener('pointerdown', start)
+    window.removeEventListener('keydown', start)
+    renderApp()
+  }
+
+  window.addEventListener('pointerdown', start, { once: true })
+  window.addEventListener('keydown', start, { once: true })
+  window.setTimeout(start, 2500)
+} else {
+  renderApp()
+}
